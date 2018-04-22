16802637872dee005fb6dc9c6965a4f282cb4977
Anton Mokrushin <anton@mokr.org>
2018-04-22 04:39:15 +0300

    add DOCKER_REGISTRY_URI to .env

--- scripts/start.sh
+++ scripts/start.sh
@@ -19,6 +19,7 @@ then
     dotenv_clear
     dotenv_add COMPOSE_FILE=docker-compose.yml:docker-compose.prod.yml
     dotenv_add VERSION=$(version_current ${PWD}/.version)
+    dotenv_add DOCKER_REGISTRY_URI=${DOCKER_REGISTRY_URI}
     dotenv_add SSHD_PUBLIC_PORT=${SSHD_PUBLIC_PORT}
 else
     echo -e "\n${TITLE_FORMAT}Environment: ${SUBTITLE_FORMAT}development ${RESET_FORMAT}\n"
@@ -26,6 +27,7 @@ else
     dotenv_add COMPOSE_FILE=docker-compose.yml:docker-compose.dev.yml
     dotenv_add VERSION=$(version_current ${PWD}/.version)
     dotenv_add YARN_CACHE_DIR=data/.cache/yarn
+    dotenv_add DOCKER_REGISTRY_URI=${DOCKER_REGISTRY_URI}
     dotenv_add SSHD_PUBLIC_PORT=${SSHD_PUBLIC_PORT}
 fi
 
